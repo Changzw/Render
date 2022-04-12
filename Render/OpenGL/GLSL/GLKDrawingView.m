f3e26bfeea33c97537bb43830a150db4b77b0bb5
czw <changzhongwei3@163.com>
2022-04-12 22:40:53 +0800

    update

--- Render/OpenGL/GLSL/GLKDrawingView.m
+++ Render/OpenGL/GLSL/GLKDrawingView.m
@@ -79,8 +79,7 @@
 }
 
 //6.开始绘制
--(void)renderLayer
-{
+-(void)renderLayer {
   //设置清屏颜色
   glClearColor(0.3f, 0.45f, 0.5f, 1.0f);
   //清除屏幕
@@ -190,15 +189,10 @@
   
   //13.从渲染缓存区显示到屏幕上
   [self.myContext presentRenderbuffer:GL_RENDERBUFFER];
-  
-  
 }
-
-
 
 //从图片中加载纹理
 - (GLuint)setupTexture:(NSString *)fileName {
-  
   //1、将 UIImage 转换为 CGImageRef
   CGImageRef spriteImage = [UIImage imageNamed:fileName].CGImage;
   
@@ -279,91 +273,68 @@
 }
 
 //5.设置FrameBuffer
--(void)setupFrameBuffer
-{
+-(void)setupFrameBuffer {
   //1.定义一个缓存区ID
   GLuint buffer;
-  
   //2.申请一个缓存区标志
   //glGenRenderbuffers(1, &buffer);
   //glGenFramebuffers(1, &buffer);
   glGenBuffers(1, &buffer);
-  
   //3.
   self.myColorFrameBuffer = buffer;
-  
   //4.
   glBindFramebuffer(GL_FRAMEBUFFER, self.myColorFrameBuffer);
-  
   /*生成帧缓存区之后，则需要将renderbuffer跟framebuffer进行绑定，
    调用glFramebufferRenderbuffer函数进行绑定到对应的附着点上，后面的绘制才能起作用
    */
-  
   //5.将渲染缓存区myColorRenderBuffer 通过glFramebufferRenderbuffer函数绑定到 GL_COLOR_ATTACHMENT0上。
   glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, self.myColorRenderBuffer);
-  
 }
 
 //4.设置RenderBuffer
--(void)setupRenderBuffer
-{
+-(void)setupRenderBuffer {
   //1.定义一个缓存区ID
   GLuint buffer;
-  
   //2.申请一个缓存区标志
   glGenRenderbuffers(1, &buffer);
-  
   //3.
   self.myColorRenderBuffer = buffer;
-  
   //4.将标识符绑定到GL_RENDERBUFFER
   glBindRenderbuffer(GL_RENDERBUFFER, self.myColorRenderBuffer);
-  
   //5.将可绘制对象drawable object's  CAEAGLLayer的存储绑定到OpenGL ES renderBuffer对象
   [self.myContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:self.myEagLayer];
-  
-  
 }
 
 //3.清空缓存区
--(void)deleteRenderAndFrameBuffer
-{
+-(void)deleteRenderAndFrameBuffer {
   /*
    buffer分为frame buffer 和 render buffer2个大类。
    其中frame buffer 相当于render buffer的管理者。
    frame buffer object即称FBO。
    render buffer则又可分为3类。colorBuffer、depthBuffer、stencilBuffer。
    */
-  
   glDeleteBuffers(1, &_myColorRenderBuffer);
   self.myColorRenderBuffer = 0;
-  
   glDeleteBuffers(1, &_myColorFrameBuffer);
   self.myColorFrameBuffer = 0;
-  
 }
 
-
 //2.设置上下文
--(void)setupContext
-{
-  //1.指定OpenGL ES 渲染API版本，我们使用2.0
-  EAGLRenderingAPI api = kEAGLRenderingAPIOpenGLES2;
-  //2.创建图形上下文
-  EAGLContext *context = [[EAGLContext alloc]initWithAPI:api];
-  //3.判断是否创建成功
+-(void)setupContext {
+  //1.指定OpenGL ES 渲染API版本，我们使用2.0.创建图形上下文
+  EAGLContext *context = [[EAGLContext alloc]initWithAPI:kEAGLRenderingAPIOpenGLES2];
+  //2.判断是否创建成功
   if (!context) {
     NSLog(@"Create context failed!");
     return;
   }
-  //4.设置图形上下文
+  //3.设置图形上下文
   if (![EAGLContext setCurrentContext:context]) {
     NSLog(@"setCurrentContext failed!");
     return;
   }
   //5.将局部context，变成全局的
   self.myContext = context;
-  
 }
 
 //1.设置图层
@@ -374,11 +345,8 @@
    重写layerClass，将CCView返回的图层从CALayer替换成CAEAGLLayer
    */
   self.myEagLayer = (CAEAGLLayer *)self.layer;
-  
   //2.设置scale
-  [self setContentScaleFactor:[[UIScreen mainScreen]scale]];
-  
-  
+  [self setContentScaleFactor:[[UIScreen mainScreen] scale]];
   //3.设置描述属性，这里设置不维持渲染内容以及颜色格式为RGBA8
   /*
    kEAGLDrawablePropertyRetainedBacking  表示绘图表面显示后，是否保留其内容。
@@ -388,23 +356,20 @@
    kEAGLColorFormatRGBA8：32位RGBA的颜色，4*8=32位
    kEAGLColorFormatRGB565：16位RGB的颜色，
    kEAGLColorFormatSRGBA8：sRGB代表了标准的红、绿、蓝，即CRT显示器、LCD显示器、投影机、打印机以及其他设备中色彩再现所使用的三个基本色素。sRGB的色彩空间基于独立的色彩坐标，可以使色彩在不同的设备使用传输中对应于同一个色彩坐标体系，而不受这些设备各自具有的不同色彩坐标的影响。
-   
-   
    */
-  self.myEagLayer.drawableProperties = [NSDictionary dictionaryWithObjectsAndKeys:@false,kEAGLDrawablePropertyRetainedBacking, kEAGLColorFormatRGBA8,kEAGLDrawablePropertyColorFormat,nil];
-  
+  self.myEagLayer.drawableProperties = [NSDictionary dictionaryWithObjectsAndKeys:
+                                        @false,kEAGLDrawablePropertyRetainedBacking,
+                                        kEAGLColorFormatRGBA8, kEAGLDrawablePropertyColorFormat,nil];
 }
 
-+(Class)layerClass
-{
++(Class)layerClass {
   return [CAEAGLLayer class];
 }
 
 
 #pragma mark --shader
 //加载shader
--(GLuint)loadShaders:(NSString *)vert Withfrag:(NSString *)frag
-{
+-(GLuint)loadShaders:(NSString *)vert Withfrag:(NSString *)frag {
   //1.定义2个零时着色器对象
   GLuint verShader, fragShader;
   //创建program
